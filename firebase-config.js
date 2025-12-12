23b238de392ffba5e5dd4fd2f749def490db5a6d
makemefaster <42447944+makemefaster@users.noreply.github.com>
2025-12-12 14:18:05 +0000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,22 +1,22 @@
-// Import the functions you need from the SDKs
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-database.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAkTwI7TbJwXPbVQ2OrKjAAIaWLvyfuudc",
-    authDomain: "sprintfutures.firebaseapp.com",
-    databaseURL: "https://sprintfutures-default-rtdb.firebaseio.com",
-    projectId: "sprintfutures",
-    storageBucket: "sprintfutures.firebasestorage.app",
-    messagingSenderId: "535060943616",
-    appId: "1:535060943616:web:d15fc5225538eaeb3459e5",
-    measurementId: "G-9XFQV3PLEV"
+  apiKey: "AIzaSyB421IaC3NQIDnYvNA7oVYgvfwYG-8aT4o",
+  authDomain: "tournament-champ.firebaseapp.com",
+  databaseURL: "https://tournament-champ-default-rtdb.firebaseio.com",
+  projectId: "tournament-champ",
+  storageBucket: "tournament-champ.firebasestorage.app",
+  messagingSenderId: "65472067494",
+  appId: "1:65472067494:web:ba7298e7378e7604354167",
+  measurementId: "G-2C3LWDC2X3"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-
-// Export the database connection to be used by other files
-export { db };
+const analytics = getAnalytics(app);
